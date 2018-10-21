0abe5c6ae50c9d9fa03c06ea45c31020ebc796ce
lxambulance <751795314@qq.com>
2018-10-21 22:13:50 +0800

    delete line,modify Geometry

--- lxACMusefulmodel/Geometry.cpp
+++ lxACMusefulmodel/Geometry.cpp
@@ -1,53 +1,66 @@
+#define sqr(x) ((x)*(x))
 const double eps=1e-8,INF=1e20,PI=3.141592653589793238462;
 inline int sgn(double x){ if (fabs(x)<=eps) return 0; return x>0?1:-1; }
 struct Point{
 	double x,y;
 	Point(double xx=0,double yy=0):x(xx),y(yy){}
 	double len(){ return sqrt(x*x+y*y); }
+	//向量的加减乘除
 	Point operator +(const Point &p)const{ return Point(x+p.x,y+p.y); }
 	Point operator -(const Point &p)const{ return Point(x-p.x,y-p.y); }
 	Point operator *(const double &f)const{ return Point(x*f,y*f); }
+	//要求f不为0
 	Point operator /(const double &f)const{ return Point(x/f,y/f); }
+	//向量的叉积与点积
 	double operator *(const Point &p)const{ return x*p.y-p.x*y; }
 	double operator ^(const Point &p)const{ return x*p.x+y*p.y; }
+	//凸包的排序
 	bool operator <(const Point &p)const{ return x<p.x||(x==p.x&&y<p.y); }
-}A,B,C,O;
+};
 typedef Point Vector;
-inline double dist(Point a,Point b){ return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)); }
-inline double cross(Point a,Point b,Point o){ return (a.x-o.x)*(b.y-o.y)-(a.y-o.y)*(b.x-o.x); }
-inline double cross(Vector v1,Vector v2){ return v1.x*v2.y-v1.y*v2.x; }
-inline double dot(Point a,Point b,Point o){ return (a.x-o.x)*(b.x-o.x)+(a.y-o.y)*(b.y-o.y); }
-inline double dot(Vector v1,Vector v2){ return v1.x*v2.x+v1.y*v2.y; }
-inline Point rot(Point p,double a){ return Point(cos(a)*p.x-sin(a)*p.y,sin(a)*p.x+cos(a)*p.y); }
-double deg(Point p){ double tmp=atan2(p.y,p.x); return tmp<0?tmp+PI*2:tmp; }
-Point project(Point o,Point p,Vector v){ double tmp=((o-p)^v)/(v^v); return p+v*tmp; }
+//函数形式的距离、叉积与点积
+double Dist(Point a,Point b){ return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)); }
+double Cross(Point a,Point b,Point o){ return (a.x-o.x)*(b.y-o.y)-(a.y-o.y)*(b.x-o.x); }
+double Cross(Vector v1,Vector v2){ return v1.x*v2.y-v1.y*v2.x; }
+double Dot(Point a,Point b,Point o){ return (a.x-o.x)*(b.x-o.x)+(a.y-o.y)*(b.y-o.y); }
+double Dot(Vector v1,Vector v2){ return v1.x*v2.x+v1.y*v2.y; }
+//向量旋转（逆时针，弧度）
+inline Point Rot(Point p,double a){ return Point(cos(a)*p.x-sin(a)*p.y,sin(a)*p.x+cos(a)*p.y); }
+//向量极角（弧度）
+double Deg(Point p){ double tmp=atan2(p.y,p.x); return tmp<0?tmp+PI*2:tmp; }
+//点p在直线上的投影，要求|v|不为0
+Point Project(Point o,Point p,Vector v){ double tmp=((o-p)^v)/(v^v); return p+v*tmp; }
+//两直线（点向式）交点，要求直线不平行
 Point lineIntersect(Point p1,Vector v1,Point p2,Vector v2){ double tmp=(v1*(p2-p1))/(v2*v1); return p2+v2*tmp; }
-double point2seg(Point p1,Point p2, Point o){
-	if (dot(o,p1,p2)<eps) return dist(p2,o);
-	else if (dot(o,p2,p1)<eps) return dist(p1,o);
-	else return fabs(cross(o,p2,p1))/dist(p1,p2);
+//点到线段的距离，要求s1!=s2
+double pointToSeg(Point s1,Point s2, Point p){
+	if (Dot(p,s1,s2)<eps) return Dist(s2,p); else if (Dot(p,s2,s1)<eps) return Dist(s1,p);
+	return fabs(Cross(p,s2,s1))/Dist(s1,s2);
 }
+//点到直线的距离
+double pointToLine(Point p,Point o,Vector v){ return fabs(Cross(p-o,v))/v.len(); }
+//线段是否相交
+inline bool segIntersect(Point a,Point b,Point c,Point d){
+	if (max(a.x,b.x)<min(c.x,d.x)||min(a.x,b.x)>max(c.x,d.x)) return false;
+	if (max(a.y,b.y)<min(c.y,d.y)||min(a.y,b.y)>max(c.y,d.y)) return false;
+	double x=Cross(a-b,c-b),y=Cross(a-b,d-b),z=Cross(c-d,a-d),w=Cross(c-d,b-d);
+	if (sgn(x)==0&&sgn(y)==0) return false;
+	if (sgn(z)==0) return true; else if (sgn(w)==0) return false;
+	if (sgn(x)*sgn(y)<=0&&sgn(z)*sgn(w)<=0) return true; else return false;
+}
+//凸包，相邻边不平行
 int ConvexHull(Point p[],int n,Point q[]){
 	sort(p+1,p+n+1);
 	int i,m=0;
-	for (i=1;i<=n;i++) {
-		while (m>1&&cross(q[m],p[i],q[m-1])<=eps) m--;
+	for (i=1;i<=n;++i) {
+		while (m>1&&Cross(q[m],p[i],q[m-1])<=eps) m--;
 		q[++m]=p[i];
 	}
 	int k=m;
-	for (i=n-1;i>0;i--) {
-		while (m>k&&cross(q[m],p[i],q[m-1])<=eps) m--;
+	for (i=n-1;i>0;--i) {
+		while (m>k&&Cross(q[m],p[i],q[m-1])<=eps) m--;
 		q[++m]=p[i];
 	}
 	if (n>1) m--;
 	return m;
-}
-
-struct L{
-	double a,b,c;
-	L(Point A=Point(),Point B=Point()){ a=B.y-A.y; b=A.x-B.x; c=-(A*B); }
-}l1,l2;
-inline Point clammer(L l,L r){ //(l.a*r.b-r.a*l.b)!=0
-	Point ans(-l.c*r.b+l.b*r.c,-l.a*r.c+l.c*r.a);
-	return ans/(l.a*r.b-r.a*l.b);
 }
