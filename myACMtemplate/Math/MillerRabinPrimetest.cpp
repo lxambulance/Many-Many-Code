8cc4ce75b9e89135b8d6dc5d5bc9ecf7bdd85e1a
lxambulance <751795314@qq.com>
2019-11-07 11:14:44 +0800

    more efficent

--- myACMtemplate/Math/MillerRabinPrimetest.cpp
+++ myACMtemplate/Math/MillerRabinPrimetest.cpp
@@ -11,29 +11,31 @@ ll qmul(ll a,ll b,ll c) {
 	return ans;
 }
 ll qpow(ll a,ll b,ll c) {
-	ll ans=1;
+	ll ans=1; a%=c;
 	while (b) {
 		if (b&1) ans=qmul(ans,a,c);
 		a=qmul(a,a,c); b>>=1;
 	}
 	return ans;
 }
-bool Miller_Rabin_test(ll a,ll b,ll n) {
+//{2,7,61}47591231;//41=4e9
+//{2,2570940,880937,610386380,4130785767};//341550071728321=3e15
+int pri[]={2,325,9375,28178,450755,9780504,1795265022};//2^64
+bool Miller_Rabin_test(ll n) {
 	if (n==2) return true;
-	else if (n==a||!(n&1)) return false;
-	while (!(b&1)) b>>=1;
-	ll tmp=qpow(a,b,n);
-	while (b!=n-1&&tmp!=n-1&&tmp!=1) { tmp=qmul(tmp,tmp,n); b<<=1; }
-	return (tmp==n-1)||((b&1)&&(tmp==1));
-}
-//{2,7,61}4759123141
-//{2,3,5,7,11,13,17}341550071728321
-//{2,3,5,7,11,13,17,19,23,29,31,37}2^64
-bool isprime(ll n){
-    int a[]={2,3,5,7,11,13,17,19,23,29,31,37};
-	for (int i=0;i<12;++i) {
-		if (n==a[i]) return true;
-		if (!Miller_Rabin_test(a[i],n-1,n)) return false;
+	if (n<2||!(n&1)) return false;
+	ll k=0,m=n-1;
+	while (!(m&1)) m>>=1,k++;
+	FOR(i,0,6) {
+		if (pri[i]>=n) return true;
+		ll x=qpow(pri[i],m,n);
+		if (x==1) continue;
+		bool flag=false;
+		FOR(j,1,k) {
+			if (x==n-1) { flag=true; break; }
+			x=qmul(x,x,n);
+		}
+		if (!flag) return false;
 	}
 	return true;
 }
